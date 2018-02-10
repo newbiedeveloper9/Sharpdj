f152b223b58774b2556c2d8ca7a32ea0181f2b80
Michał <kontogry96@gmail.com>
2018-02-10 17:10:13 +0100

    sqlhelper

---
+++ servertcp/Sql/SqlHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace servertcp.Sql
+{
+    public class SqlHelper
+    {
+        public static int ExecuteNonQuery(string stringConnection, string command, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(stringConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    conn.Open();
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static SqlDataReader ExecuteDataReader(string stringConnection, string command, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(stringConnection);
+            using (SqlCommand cmd = new SqlCommand(command, conn))
+            {
+                conn.Open();
+                cmd.Parameters.AddRange(parameters);
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+        }
+    }
+}
---
+++ servertcp/Sql/SqlUserCommands.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace servertcp.Sql
+{
+    class SqlUserCommands
+    {
+        public void CreateUser(string strConnection, string login, string password, string salt, string username)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@Login", login));
+            parameters.Add(new SqlParameter("@Password", password));
+            parameters.Add(new SqlParameter("@Salt", salt));
+            parameters.Add(new SqlParameter("@Username", username));
+
+            SqlHelper.ExecuteNonQuery(strConnection, "INSERT INTO Users (Login, Username, Password, Salt) " + 
+                                                     "VALUES('@Login', '@Username', '@Password', '@Salt')", parameters.ToArray());
+        }
+
+        public bool CheckPassword(string strConnection, string password, string login)
+        {
+            SqlParameter parameter = new SqlParameter("@Login", login);
+            using (var reader = SqlHelper.ExecuteDataReader(strConnection, "SELECT Users.Password" +
+                                                                      "FROM Users WHERE @Login = Users.Login", parameter))
+                return reader["Password"] == password;
+        }
+
+        public string GetSalt(string strConnection, string login)
+        {
+            SqlParameter parameter = new SqlParameter("@Login", login);
+            using (var reader = SqlHelper.ExecuteDataReader(strConnection, "SELECT Users.Salt " +
+                                                                           "FROM Users WHERE @Login = Users.Login", parameter))
+                return reader["Salt"].ToString();
+        }
+
+
+    }
+}
