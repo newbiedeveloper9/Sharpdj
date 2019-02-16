bf5ad32f5d9d2569a035bd6fc117db574161609b
Michał Wojciechowski <newbiedeveloper9@users.noreply.github.com>
2019-02-16 17:10:18 +0100

    Chat is scrolling down automatically from now

--- sharpdj/Views/SubViews/MainViewComponents/RoomView.xaml.cs
+++ sharpdj/Views/SubViews/MainViewComponents/RoomView.xaml.cs
@@ -11,5 +11,34 @@ namespace SharpDj.Views.SubViews.MainViewComponents
         {
             InitializeComponent();
         }
+
+        private bool AutoScroll = true;
+        /*
+         * https://stackoverflow.com/a/19315242
+         */
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (ScrollViewer.VerticalOffset == ScrollViewer.ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set auto-scroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset auto-scroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and auto-scroll mode set
+                // Autoscroll
+                ScrollViewer.ScrollToVerticalOffset(ScrollViewer.ExtentHeight);
+            }
+        }
     }
 }
